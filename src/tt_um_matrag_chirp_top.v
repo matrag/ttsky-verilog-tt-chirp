5a5ad6ee5780c49cb92fab22544d5bf1a2e5507e
matrag <49486126+matrag@users.noreply.github.com>
2025-07-27 20:20:09 +0200

    Update tt_um_matrag_chirp_top.v

--- src/tt_um_matrag_chirp_top.v
+++ src/tt_um_matrag_chirp_top.v
@@ -6,10 +6,10 @@
 `default_nettype none
 
 module tt_um_matrag_chirp_top (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
+    input  wire [7:0] ui_in,    // Dedicated inputs - ui_in[0] UART RX
+    output wire [7:0] uo_out,   // Dedicated outputs - Out 8 bit data: uo_out[7:0]
+    input  wire [7:0] uio_in,   // IOs: Input path - unused
+    output wire [7:0] uio_out,  // IOs: Output path - Done signal from chirp generator: uio_out[0]
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled, always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -42,12 +42,12 @@ chirpmod #(
   );
 
 //-------------------Unused Out Ports assigned to '0'-------------------
-//-----All output pins must be assigned. If not used, assign to '0'-----
-assign uio_out  [7:1]   = 0;
+//-----All output pins must be assigned. If not used, assign to '1'-----
+assign uio_out  [7:1]   = 7'b1;
 //-------------------IO Ports assigned to Output ('1')------------------
-assign uio_oe   [7:0]   = 1;
+assign uio_oe   [7:0]   = 8'b1;
 
 // List all unused inputs to prevent warnings
- wire _unused = &{ena, ui_in[7:1], uio_in[7:0], 1'b0};
+    wire _unused = &{ena, ui_in[7:1], uio_in[7:0], 1'b1};
 
 endmodule
