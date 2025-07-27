1456562d957a3e25034e1ae01e3294e6b8d48fde
matrag <49486126+matrag@users.noreply.github.com>
2025-07-27 18:26:27 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -14,15 +14,25 @@ module tb ();
   end
 
   // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
+  //reg clk;
+  //reg rst_n;
+  //reg ena;
+  //reg [7:0] ui_in;
+  //reg [7:0] uio_in;
+  //wire [7:0] uo_out;
+  //wire [7:0] uio_out;
+  //wire [7:0] uio_oe;
+
+   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+  reg clk;
+  reg rst_n;
+  reg ena = 1;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+   
   // Replace tt_um_example with your module name:
   tt_um_matrag_chirp_top uut(
       .ui_in  (ui_in),    // Dedicated inputs
