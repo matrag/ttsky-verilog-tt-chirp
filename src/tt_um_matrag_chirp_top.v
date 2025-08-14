a007aae61906a92b4dc256bbfc9db321e60f5265
matrag <49486126+matrag@users.noreply.github.com>
2025-08-14 20:55:53 +0200

    Update tt_um_matrag_chirp_top.v

--- src/tt_um_matrag_chirp_top.v
+++ src/tt_um_matrag_chirp_top.v
@@ -30,26 +30,19 @@ chirpmod #(
     .i_clk   (clk),   //!Input clock: 10 MHz
     .i_rst_n (rst_n),   //!Input reset active Low
     .i_rx    (ui_in[0]),   //!Input UART rx (9600 bps) //UART RX input line: ui_in[0] port
-    //.o_done_n(w_o_done),   //!Output done active Low
-    .o_done_n(uio_out[0]),
+    .o_done_n(uio_out[0]), //!Output done active Low
     .o_data  (uo_out)    //!Output bus data (8 bit)
   );
 
-    clk_generator clk_gen_inst
-    (
-        .i_rst_n(rst_n),
-        .o_clk  (uio_out[1])
-    );
-    
-
-    //-------------------IO Ports assigned to Output ('1')------------------
-    assign uio_oe   [7:0]   = 8'b0000_0011; //enable used uio outputs
-    assign uio_out  [7:2]   = 6'b0000_00;   //set other to zero
+     //-------------------IO Ports assigned to Output ('1')------------------
+    assign uio_oe   [7:0]   = 8'b0000_0001; //enable used uio outputs
+    assign uio_out  [7:2]   = 7'b0000_000;   //set other to zero
 
     // List all unused inputs to prevent warnings
     wire _unused = &{ena, uio_oe[7:0], uio_out[7:1], ui_in[7:1], uio_in[7:0], 1'b0};
 
 endmodule
+
 
 
 
