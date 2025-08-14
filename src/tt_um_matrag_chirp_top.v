bb2d5b0d65b212d37fa981174473147aeb1b12b1
matrag <49486126+matrag@users.noreply.github.com>
2025-08-14 14:45:40 +0200

    Update tt_um_matrag_chirp_top.v

--- src/tt_um_matrag_chirp_top.v
+++ src/tt_um_matrag_chirp_top.v
@@ -20,8 +20,8 @@ module tt_um_matrag_chirp_top (
 //------Wires to connect Ports to design instantiation------------------
 //----------------------------------------------------------------------
 wire        w_i_rx     = ui_in[0];       //UART RX input line: ui_in[0] port
-wire        w_o_done;
-assign      uio_out[0] = w_o_done;     //Done signal from chirp generator: uio_out[0]
+//wire        w_o_done;
+//assign      uio_out[0] = w_o_done;     //Done signal from chirp generator: uio_out[0]
 wire [7:0]  w_o_data;   
 assign      uo_out = w_o_data;         //Out 8 bit data: uo_out[7:0]
 
@@ -39,18 +39,22 @@ chirpmod #(
     .i_clk   (clk),   //!Input clock: 10 MHz
     .i_rst_n (rst_n),   //!Input reset active Low
     .i_rx    (w_i_rx),   //!Input UART rx (9600 bps)
-    .o_done_n(w_o_done),   //!Output done active Low
+    //.o_done_n(w_o_done),   //!Output done active Low
+    .o_done_n(),
     .o_data  (w_o_data)    //!Output bus data (8 bit)
   );
 
     //-------------------IO Ports assigned to Output ('1')------------------
-    assign uio_oe   [7:0]   = 8'b1111_1111; //enabe uio_out[0]
-    assign uio_out  [7:1]   = 7'b0000_000;
+    //assign uio_oe   [7:0]   = 8'b1111_1111; //enabe uio_out[0]
+    //assign uio_out  [7:1]   = 7'b0000_000;
+    assign uio_oe   [7:0]   = 8'b0000_0000;
+    assign uio_out  [7:0]   = 8'b0000_0000;
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_oe[7:1], uio_out[7:1], ui_in[7:1], uio_in[7:0], 1'b0};
+    wire _unused = &{ena, uio_oe[7:0], uio_out[7:0], ui_in[7:1], uio_in[7:0], 1'b0};
 
 endmodule
+
 
 
 
